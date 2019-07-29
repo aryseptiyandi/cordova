41a367155227e7f75aa6d5d3b63caa75838acb86
aryseptiyandi <48472304+aryseptiyandi@users.noreply.github.com>
2019-07-29 23:44:59 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -2,10 +2,11 @@
  include "db.php";
  if(isset($_POST['insert']))
  {
+ $nmpemesan=$_POST['nmpemesan'];
  $tpkamar=$_POST['tpkamar'];
  $durasi=$_POST['durasi'];
  $harga=$_POST['harga'];
- $q=mysqli_query($con,"INSERT INTO `pemesanan` (`tpkamar`,`durasi`,`harga`) VALUES ('$tpkamar','$durasi','$harga')");
+ $q=mysqli_query($con,"INSERT INTO `pemesanan` (`nmpemesan`,`tpkamar`,`durasi`,`harga`) VALUES ('$nmpemesan','$tpkamar','$durasi','$harga')");
  if($q)
   echo "success";
  else
