b5cf169770e3276a688dfd89460210081921004e
aryseptiyandi <48472304+aryseptiyandi@users.noreply.github.com>
2019-07-30 13:27:59 +0700

    Update update.php

--- www/update.php
+++ www/update.php
@@ -2,12 +2,13 @@
  include "db.php";
  if(isset($_POST['update']))
  {
- $Id=$_POST['Id'];    
+ $Id=$_POST['id'];    
  $nmpemesan=$_POST['nmpemesan'];
+ $no_ktp=$_POST['no_ktp'];
  $tpkamar=$_POST['tpkamar'];
  $durasi=$_POST['durasi'];
  $harga=$_POST['harga'];
- $q=mysqli_query($con,"UPDATE `pemesanan` SET `nmpemesan`='$nmpemesan',`tpkamar`='$tpkamar',`durasi`='$durasi',`harga`='$harga' where `Id`='$Id'");
+ $q=mysqli_query($con,"UPDATE `pemesanan` SET `nmpemesan`='$nmpemesan',`no_ktp`='$no_ktp',`tpkamar`='$tpkamar',`durasi`='$durasi',`harga`='$harga' where `id`='$Id'");
  if($q)
  echo "sukses";
  else
